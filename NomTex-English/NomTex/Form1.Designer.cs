34680e8b32a9eb48ed99ff80f57d4a905c2da94e
gamerYazilimci67 <abdulbakisari54@gmail.com>
2024-10-04 00:29:43 +0300

    Move files

---
+++ NomTex-English/NomTex/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace NomTex
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            menuStrip1 = new MenuStrip();
+            dosyaToolStripMenuItem = new ToolStripMenuItem();
+            yeniToolStripMenuItem = new ToolStripMenuItem();
+            açToolStripMenuItem = new ToolStripMenuItem();
+            kaydetToolStripMenuItem = new ToolStripMenuItem();
+            düzenleToolStripMenuItem = new ToolStripMenuItem();
+            bulToolStripMenuItem = new ToolStripMenuItem();
+            kopyalaToolStripMenuItem1 = new ToolStripMenuItem();
+            yapıştırToolStripMenuItem1 = new ToolStripMenuItem();
+            kesToolStripMenuItem1 = new ToolStripMenuItem();
+            richTextBox1 = new RichTextBox();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            kopyalaToolStripMenuItem = new ToolStripMenuItem();
+            yapıştırToolStripMenuItem = new ToolStripMenuItem();
+            kesToolStripMenuItem = new ToolStripMenuItem();
+            pictureBox1 = new PictureBox();
+            trackBar1 = new TrackBar();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            comboBox2 = new ComboBox();
+            menuStrip1.SuspendLayout();
+            contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.White;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { dosyaToolStripMenuItem, düzenleToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            dosyaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { yeniToolStripMenuItem, açToolStripMenuItem, kaydetToolStripMenuItem });
+            dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            dosyaToolStripMenuItem.Size = new Size(37, 20);
+            dosyaToolStripMenuItem.Text = "File";
+            // 
+            // yeniToolStripMenuItem
+            // 
+            yeniToolStripMenuItem.Name = "yeniToolStripMenuItem";
+            yeniToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.N;
+            yeniToolStripMenuItem.Size = new Size(146, 22);
+            yeniToolStripMenuItem.Text = "New";
+            yeniToolStripMenuItem.Click += yeniToolStripMenuItem_Click;
+            // 
+            // açToolStripMenuItem
+            // 
+            açToolStripMenuItem.Name = "açToolStripMenuItem";
+            açToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.O;
+            açToolStripMenuItem.Size = new Size(146, 22);
+            açToolStripMenuItem.Text = "Open";
+            açToolStripMenuItem.Click += açToolStripMenuItem_Click;
+            // 
+            // kaydetToolStripMenuItem
+            // 
+            kaydetToolStripMenuItem.Name = "kaydetToolStripMenuItem";
+            kaydetToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.S;
+            kaydetToolStripMenuItem.Size = new Size(146, 22);
+            kaydetToolStripMenuItem.Text = "Save";
+            kaydetToolStripMenuItem.Click += kaydetToolStripMenuItem_Click;
+            // 
+            // düzenleToolStripMenuItem
+            // 
+            düzenleToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { bulToolStripMenuItem, kopyalaToolStripMenuItem1, yapıştırToolStripMenuItem1, kesToolStripMenuItem1 });
+            düzenleToolStripMenuItem.Name = "düzenleToolStripMenuItem";
+            düzenleToolStripMenuItem.Size = new Size(39, 20);
+            düzenleToolStripMenuItem.Text = "Edit";
+            // 
+            // bulToolStripMenuItem
+            // 
+            bulToolStripMenuItem.Name = "bulToolStripMenuItem";
+            bulToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.F;
+            bulToolStripMenuItem.Size = new Size(180, 22);
+            bulToolStripMenuItem.Text = "Find";
+            bulToolStripMenuItem.Click += bulToolStripMenuItem_Click;
+            // 
+            // kopyalaToolStripMenuItem1
+            // 
+            kopyalaToolStripMenuItem1.Name = "kopyalaToolStripMenuItem1";
+            kopyalaToolStripMenuItem1.ShortcutKeys = Keys.Control | Keys.C;
+            kopyalaToolStripMenuItem1.Size = new Size(180, 22);
+            kopyalaToolStripMenuItem1.Text = "Copy";
+            kopyalaToolStripMenuItem1.Click += kopyalaToolStripMenuItem1_Click;
+            // 
+            // yapıştırToolStripMenuItem1
+            // 
+            yapıştırToolStripMenuItem1.Name = "yapıştırToolStripMenuItem1";
+            yapıştırToolStripMenuItem1.ShortcutKeys = Keys.Control | Keys.V;
+            yapıştırToolStripMenuItem1.Size = new Size(180, 22);
+            yapıştırToolStripMenuItem1.Text = "Paste";
+            yapıştırToolStripMenuItem1.Click += yapıştırToolStripMenuItem1_Click;
+            // 
+            // kesToolStripMenuItem1
+            // 
+            kesToolStripMenuItem1.Name = "kesToolStripMenuItem1";
+            kesToolStripMenuItem1.ShortcutKeys = Keys.Control | Keys.X;
+            kesToolStripMenuItem1.Size = new Size(180, 22);
+            kesToolStripMenuItem1.Text = "Cut";
+            kesToolStripMenuItem1.Click += kesToolStripMenuItem1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.ContextMenuStrip = contextMenuStrip1;
+            richTextBox1.Location = new Point(0, 27);
+            richTextBox1.MaximumSize = new Size(1920, 950);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(800, 375);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { kopyalaToolStripMenuItem, yapıştırToolStripMenuItem, kesToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(145, 70);
+            // 
+            // kopyalaToolStripMenuItem
+            // 
+            kopyalaToolStripMenuItem.Name = "kopyalaToolStripMenuItem";
+            kopyalaToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.C;
+            kopyalaToolStripMenuItem.Size = new Size(144, 22);
+            kopyalaToolStripMenuItem.Text = "Copy";
+            kopyalaToolStripMenuItem.Click += kopyalaToolStripMenuItem_Click;
+            // 
+            // yapıştırToolStripMenuItem
+            // 
+            yapıştırToolStripMenuItem.Name = "yapıştırToolStripMenuItem";
+            yapıştırToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.V;
+            yapıştırToolStripMenuItem.Size = new Size(144, 22);
+            yapıştırToolStripMenuItem.Text = "Paste";
+            yapıştırToolStripMenuItem.Click += yapıştırToolStripMenuItem_Click;
+            // 
+            // kesToolStripMenuItem
+            // 
+            kesToolStripMenuItem.Name = "kesToolStripMenuItem";
+            kesToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.X;
+            kesToolStripMenuItem.Size = new Size(144, 22);
+            kesToolStripMenuItem.Text = "Cut";
+            kesToolStripMenuItem.Click += kesToolStripMenuItem_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(117, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(35, 24);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Cursor = Cursors.NoMoveHoriz;
+            trackBar1.Location = new Point(648, 405);
+            trackBar1.Maximum = 50;
+            trackBar1.Minimum = 9;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(152, 45);
+            trackBar1.TabIndex = 3;
+            trackBar1.Value = 9;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.FlatStyle = FlatStyle.Popup;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.Location = new Point(581, 418);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Font Size:";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(379, 415);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 7;
+            comboBox1.SelectedValueChanged += comboBox1_SelectedValueChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.FlatStyle = FlatStyle.Popup;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.Location = new Point(338, 418);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Font:";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Regular", "Bold", "Italic", "Underline", "Strikeout" });
+            comboBox2.Location = new Point(506, 415);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(69, 23);
+            comboBox2.TabIndex = 9;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox2);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(trackBar1);
+            Controls.Add(pictureBox1);
+            Controls.Add(richTextBox1);
+            Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "NomTex ";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem dosyaToolStripMenuItem;
+        private ToolStripMenuItem yeniToolStripMenuItem;
+        private ToolStripMenuItem açToolStripMenuItem;
+        private ToolStripMenuItem kaydetToolStripMenuItem;
+        private RichTextBox richTextBox1;
+        private PictureBox pictureBox1;
+        private TrackBar trackBar1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Label label3;
+        private ToolStripMenuItem düzenleToolStripMenuItem;
+        private ToolStripMenuItem bulToolStripMenuItem;
+        private ComboBox comboBox2;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem kopyalaToolStripMenuItem;
+        private ToolStripMenuItem yapıştırToolStripMenuItem;
+        private ToolStripMenuItem kesToolStripMenuItem;
+        private ToolStripMenuItem kopyalaToolStripMenuItem1;
+        private ToolStripMenuItem yapıştırToolStripMenuItem1;
+        private ToolStripMenuItem kesToolStripMenuItem1;
+    }
+}
